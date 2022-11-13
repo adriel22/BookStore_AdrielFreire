e690f160a3c627d8ef32e09b4c6e0f39910260b2
adriel22 <adriel.arruda@yahoo.com.br>
2022-11-13 19:55:40 -0300

    First Commit

---
+++ BookStore_AdrielFreire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookStore_AdrielFreire
+//
+//  Created by Adriel Freire on 13/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookStore_AdrielFreireUITests/BookStore_AdrielFreireUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BookStore_AdrielFreireUITestsLaunchTests.swift
+//  BookStore_AdrielFreireUITests
+//
+//  Created by Adriel Freire on 13/11/22.
+//
+
+import XCTest
+
+final class BookStore_AdrielFreireUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
